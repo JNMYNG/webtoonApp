b07e23e347e7df626d64c2107b7da51922b6c1dd
JNMYNG <jmj00327@gmail.com>
2023-07-20 19:26:30 +0900

    Add: api-service.dart

---
+++ lib/models/webtoon_model.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+
+class WebtoonModel {
+  final String title, thumb, id;
+
+  WebtoonModel.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        thumb = json['thumb'],
+        id = json['id'];
+}
